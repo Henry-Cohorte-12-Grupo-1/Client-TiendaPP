d7542409cb706acd0b166b7c896b06fd57734c26
tomasggarcia <tomy.garcia@hotmail.com>
2021-06-08 15:58:26 -0300

    form added into productForm component

--- src/components/Client/Client.tsx
+++ src/components/Client/Client.tsx
@@ -1,7 +1,10 @@
+import {Link} from 'react-router-dom'
+
 function Client (){
     return (
         <div>
             <h1>Funko</h1>
+            <Link to='/client/create'>X</Link>
         </div>
     )
 }
--- src/components/ProductForm/ProductForm.tsx
+++ src/components/ProductForm/ProductForm.tsx
@@ -1,7 +1,50 @@
-function ProductForm (){
-    return(
+import { useState } from 'react'
+import IProduct from '../../interfaces/product'
+
+function ProductForm() {
+    var [genres, setGenres] = useState([])
+    // let [nameInput, setNameInput] = useState<string>('')
+    let [product, setProduct] = useState<IProduct>({
+        name: '',
+        description: '',
+        price: 0,
+    })
+
+    const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+        if (event.target) {
+            let tName = (event.target as HTMLButtonElement).name
+            let tValue = (event.target as HTMLButtonElement).value
+            // console.log(tName,tValue)
+            setProduct({
+                ...product,
+                [tName]:tValue
+            })
+        }
+    }
+
+    const imageChangeHandler = (event: React.FormEvent<HTMLInputElement>) => {
+        console.log(event)
+    }
+
+    const handleSubmit = () => {
+        
+    }
+
+    return (
         <div>
             <h1>FORM</h1>
+            <form>
+                <label>Product Name</label>
+                <input name='name' onBlur={handleChange} /><br/>
+                <label>Description</label>
+                <input name='description' onBlur={handleChange} /><br/>
+                <label>Price</label>
+                <input name='price' onBlur={handleChange} /><br/>
+                <label>Image</label>
+                <input type='file' name='image' onChange={imageChangeHandler}/>
+                <button onClick={handleSubmit}>Upload</button>
+            </form>
+            {/* <img src={}></img> */}
         </div>
     )
 }
