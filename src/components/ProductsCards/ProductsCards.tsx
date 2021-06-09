eb005811b67982f59a017f070d8f0e52d8e6d58a
= <violetafuentes2002@gmail.com>
2021-06-09 20:17:49 -0300

    puse a la imagen dentro de un div

--- src/components/ProductsCards/ProductsCards.tsx
+++ src/components/ProductsCards/ProductsCards.tsx
@@ -5,7 +5,9 @@ function ProductsCards(props: {name: string; price: number; image: string}){
 
     return(
         <div className="card" id='firstDiv'>
+            <div id='imageSize'>
             <img className="card-img-top" src={props.image} alt='image not found' id='image' />
+            </div>
             <div className="card-body">
                 <h5 className="card-title">{props.name}</h5>
                 <p className="card-text">${props.price}</p>
