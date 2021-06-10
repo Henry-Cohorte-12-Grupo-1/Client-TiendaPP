66949c6cfb7db249c5d39a4869628f586cf3c0ff
Pedro <pedro.maresc@gmail.com>
2021-06-10 12:22:44 -0300

    Added favicons

--- src/components/ProductDetails/ProductDetails.tsx
+++ src/components/ProductDetails/ProductDetails.tsx
@@ -23,7 +23,7 @@ function ProductDetails(props: Props) {
             await dispatch(productInfo(id));
         })()
     }, []) // eslint-disable-line react-hooks/exhaustive-deps
-
+    console.log(details)
     return (
         <div>
             <h4>
@@ -32,6 +32,54 @@ function ProductDetails(props: Props) {
             <img src={details.image} alt="" />
             <h4>${details.price}</h4>
             <p>{details.description}</p>
+            <div className="container">
+                <h2 className="text-center">Bootstrap 4 User Rating Form / Comment Form</h2>
+
+                <div className="card">
+                    <div className="card-body">
+                        <div className="row">
+                            <div className="col-md-2">
+                                <img src="https://image.ibb.co/jw55Ex/def_face.jpg" className="img img-rounded img-fluid" />
+                                <p className="text-secondary text-center">15 Minutes Ago</p>
+                            </div>
+                            <div className="col-md-10">
+                                <p>
+                                    <a className="float-left" href="https://maniruzzaman-akash.blogspot.com/p/contact.html"><strong>Maniruzzaman Akash</strong></a>
+                                    <span className="float-right"><i className="text-warning fa fa-star"></i></span>
+                                    <span className="float-right"><i className="text-warning fa fa-star"></i></span>
+                                    <span className="float-right"><i className="text-warning fa fa-star"></i></span>
+                                    <span className="float-right"><i className="text-warning fa fa-star"></i></span>
+
+                                </p>
+                                <div className="clearfix"></div>
+                                <p>Lorem Ipsum is simply dummy text of the pr make  but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                <p>
+                                    <a className="float-right btn btn-outline-primary ml-2"> <i className="fa fa-reply"></i> Reply</a>
+                                    <a className="float-right btn text-white btn-danger"> <i className="fa fa-heart"></i> Like</a>
+                                </p>
+                            </div>
+                        </div>
+                        <div className="card card-inner">
+                            <div className="card-body">
+                                <div className="row">
+                                    <div className="col-md-2">
+                                        <img src="https://image.ibb.co/jw55Ex/def_face.jpg" className="img img-rounded img-fluid" />
+                                        <p className="text-secondary text-center">15 Minutes Ago</p>
+                                    </div>
+                                    <div className="col-md-10">
+                                        <p><a href="https://maniruzzaman-akash.blogspot.com/p/contact.html"><strong>Maniruzzaman Akash</strong></a></p>
+                                        <p>Lorem Ipsum is simply dummy text of the pr make  but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                        <p>
+                                            <a className="float-right btn btn-outline-primary ml-2">  <i className="fa fa-reply"></i> Reply</a>
+                                            <a className="float-right btn text-white btn-danger"> <i className="fa fa-heart"></i> Like</a>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
