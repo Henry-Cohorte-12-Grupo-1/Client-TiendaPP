ee6e21145ac96418fe56d99c1a1d0b129207ae10
Dario Santillan <santillandario823@gmail.com>
2021-06-10 13:41:29 -0300

    form responsive, fix boton delete carrousel

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import "../../styles/App/App.css";
+// import "../../styles/App/App.css";
 import { Route, RouteComponentProps, Switch } from "react-router-dom";
 
 
--- src/components/ProductForm/ProductForm.tsx
+++ src/components/ProductForm/ProductForm.tsx
@@ -1,6 +1,6 @@
 import React, { useState, SyntheticEvent, useEffect } from 'react'
 import IProduct from '../../interfaces/product'
-import { Cloudinary, CloudinaryImage } from '@cloudinary/base'
+// import { Cloudinary, CloudinaryImage } from '@cloudinary/base'
 import { useHistory } from 'react-router-dom'
 // import { fill } from '@cloudinary/base/actions/resize'
 import axios from 'axios'
@@ -111,74 +111,82 @@ function ProductForm() {
     }
 
 
-    function handleDelete (i:number){
+    function handleDelete(i: number) {
         console.log(i)
         console.log(imagesName)
         console.log(imagesName[i])
-        setImagesName(imagesName.filter(image => (image!==imagesName[i])))
-
+        setImagesName(imagesName.filter(image => (image !== imagesName[i])))
     }
 
     return (
-        <Jumbotron className='bg-primary' style={{ width: '50%', margin: '75px auto' }}>
+        <Container>
             <h1 className='text-secondary'>
                 Crear producto
             </h1>
 
-            <Form>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label className='text-secondary'>Nombre del producto</Form.Label>
-                    <Form.Control className='label-success' type='input' placeholder="Name" name='name' onBlur={handleChange} />
-                    {errors?.name ? <Form.Text className="text-muted">
-                        Nombre no puede estar vacio
+            <Form className='bg-warning p-5 rounded'>
+                <Row>
+                    <Col md>
+                        <Form.Group controlId="name">
+                            <Form.Label className='text-secondary'>Nombre del producto</Form.Label>
+                            <Form.Control className='label-success' type='input' placeholder="Name" name='name' onBlur={handleChange} />
+                            {errors?.name ? <Form.Text className="text-muted">
+                                Nombre no puede estar vacio
                     </Form.Text> : <Form.Text className="text-muted">&#160;</Form.Text>}
-                </Form.Group>
+                        </Form.Group>
 
-                <Form.Group controlId="exampleForm.ControlTextarea1">
-                    <Form.Label className='text-secondary'>Descripción</Form.Label>
-                    <Form.Control as="textarea" rows={3} name='description' placeholder="Description" onBlur={handleChange} />
-                    {errors?.description ? <Form.Text className="text-muted">
-                        La descripcion no puede estar vacia
+                        <Form.Group controlId="description">
+                            <Form.Label className='text-secondary'>Descripción</Form.Label>
+                            <Form.Control as="textarea" rows={3} name='description' placeholder="Description" onBlur={handleChange} />
+                            {errors?.description ? <Form.Text className="text-muted">
+                                La descripcion no puede estar vacia
                     </Form.Text> : <Form.Text className="text-muted">&#160;</Form.Text>}
-                </Form.Group>
+                        </Form.Group>
 
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label className='text-secondary'>Precio</Form.Label>
-                    <Form.Control type='input' placeholder="$" name='price' onBlur={handleChange} />
-                    {errors?.price ? <Form.Text className='text-secondary'>
-                        Debe indicar un precio
+                        <Form.Group controlId="price">
+                            <Form.Label className='text-secondary'>Precio</Form.Label>
+                            <Form.Control type='input' placeholder="$" name='price' onBlur={handleChange} />
+                            {errors?.price ? <Form.Text className='text-muted'>
+                                Debe indicar un precio
                     </Form.Text> : <Form.Text className='text-secondary'>&#160;</Form.Text>}
-                </Form.Group>
+                        </Form.Group>
+                    </Col>
 
+                    <Col md>
+                        <Form.File id="image">
+                            {/* <Form.File.Label>Regular file input</Form.File.Label> */}
+                            <Form.File.Input accept="image/png, image/jpeg" name='image' onChange={imageChangeHandler} />
+                        </Form.File>
+                        {imagesName.length > 0 ?
+                            <Carousel>
+                                {imagesName.map((name, i) => (
+                                    <Carousel.Item key={i}>
+                                        <Col >
+                                            <Button className="boton-prueb btn-secondary" onClick={() => handleDelete(i)}>X</Button>
+                                        </Col>
+                                        <img
+                                            key={i}
+                                            className="d-block w-100"
+                                            src={`http://res.cloudinary.com/tiendapp/image/upload/w_400,h_300,c_scale/${name}`}
+                                            alt="First slide"
+                                        />
+                                    </Carousel.Item>
+                                )
+                                )}
+                            </Carousel> :
+                            null}
+                    </Col>
+                </Row>
 
-                <Button variant="secondary" type="submit" onClick={handleSubmit}>
-                    Enviar
-                </Button>
+                <Row>
+                    <Col className="text-center" md>
+                        <Button className="m-5 w-25" variant="secondary" type="submit" onClick={handleSubmit}>Enviar</Button>
+                    </Col>
+                </Row>
+
             </Form>
 
-            <div className="mb-3">
-                <Form.File id="formcheck-api-regular">
-                    {/* <Form.File.Label>Regular file input</Form.File.Label> */}
-                    <Form.File.Input style={{color:'grey'}} accept="image/png, image/jpeg" name='image' onChange={imageChangeHandler} />
-                </Form.File>
-            </div>
-
-            <Carousel>
-                {imagesName.map((name, i) => (
-                    <Carousel.Item key={i}>
-                        <img
-                            key={i}
-                            className="d-block w-100"
-                            src={`http://res.cloudinary.com/tiendapp/image/upload/w_300,h_130,c_scale/${name}`}
-                            alt="First slide"
-                        />
-                        <button onClick={() => handleDelete(i)}>X</button>
-                    </Carousel.Item>
-                )
-                )}
-
-            </Carousel>
-        </Jumbotron>
+        </Container>
     )
 }
 
