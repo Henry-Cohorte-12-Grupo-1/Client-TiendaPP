b0e6e56d38a948470c7e386060857910c338daaa
tomasggarcia <tomy.garcia@hotmail.com>
2021-06-12 19:06:26 -0300

    admin with categories functionality finished

--- src/components/Admin/admin.tsx
+++ src/components/Admin/admin.tsx
@@ -1,25 +1,34 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { send } from "process";
+import React, { useEffect, useState } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
-
 
 function Admin() {
     const [category, setCategory] = useState<string>()
     const [categories, setCategories] = useState<string[]>([])
+    const [initialCategories, setInitial] = useState<string[]>([])
+    const [deleteCategories, setDeleteCategories] = useState<string[]>([])
 
     useEffect(() => {
         (async () => {
             var resp = await axios.get('http://localhost:3001/categories')
             var categoriesArray: string[] = resp.data.map((category: any) => category.name)
             setCategories(categoriesArray)
+            setInitial(categoriesArray)
             console.log(categoriesArray)
         })()
     }, [])
 
     const handleSubmit = async (event: React.FormEvent<any>) => {
-        const response = await axios.put('http://localhost:3001/categories', categories)
-            .catch(() => alert('No se creo el producto'))
-        console.log(response)
+        event.preventDefault()
+        let sendCategories:string[] = []
+        categories.map(category => initialCategories.includes(category)?null:sendCategories.push(category))
+        console.log(sendCategories.join(' - '))
+        console.log(deleteCategories.join(' - '))
+
+        // const response = await axios.put('http://localhost:3001/categories', categories)
+        //     .catch(() => alert('No se creo el producto'))
+        // console.log(response)
     }
 
     const handleCategoryChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
@@ -28,22 +37,26 @@ function Admin() {
 
     const addCategory = (event: any) => {
         event.preventDefault()
-        if (category) {
+        if (category && !categories.includes(category)){
             setCategories([...categories, category])
         }
     }
 
-    function handleDelete(i: number) {
-        setCategories(categories.filter(category => (category !== categories[i])))
+    const handleDelete = (event: any) => {
+        setCategories(categories.filter(category => (category !== event.target.value)))
+        // console.log(initialCategories)
+        if (!deleteCategories.includes(event.target.value) && initialCategories.includes(event.target.value)) {
+            setDeleteCategories([...deleteCategories, event.target.value])
+        }
     }
-
+    
     return (
         <Container>
             <Form className='bg-warning p-5 rounded'>
                 <Form.Label className='text-secondary'>Admin Dashboard</Form.Label>
                 <Row>
                     <Col>
-                    <br></br>
+                        <br></br>
                         <Form.Label className='text-secondary'>Add Category</Form.Label>
                         <Form.Control type='input' placeholder="New..." name='category' onChange={handleCategoryChange} />
                     </Col>
@@ -54,7 +67,7 @@ function Admin() {
                 <Form.Label className='text-secondary'>Categories</Form.Label>
                 <Form.Control as="select" multiple >
                     {categories.map((category, i) => (
-                        <option value={i} onDoubleClick={() => handleDelete(i)}>{category}</option>
+                        <option value={category} onDoubleClick={handleDelete}>{category}</option>
                     ))}
                 </Form.Control>
                 <Button className="m-5 w-25" variant="secondary" type="submit" onClick={handleSubmit} >Save</Button>
