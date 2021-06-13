b4ce9a79c4d029c2916cc9b3b7a67f1c19165f7b
Pedro <pedro.maresc@gmail.com>
2021-06-13 04:41:53 -0300

    cards now have a default image in case there's nothing to show

--- src/components/ProductsCards/ProductsCards.tsx
+++ src/components/ProductsCards/ProductsCards.tsx
@@ -9,7 +9,7 @@ function ProductsCards(props: { name: string; price: number; image: string, prod
     return (
         <div className="card" id='firstDiv'>
             <div id='imageSize'>
-                <img className="card-img-top" src={props.images && props.images.length ? (`http://res.cloudinary.com/tiendapp/image/upload/w_400,h_300,c_scale/${props.images[0].imageId}`) : ""} alt='not found' id='image' />
+                <img className="card-img-top" src={props.images && props.images.length ? (`http://res.cloudinary.com/tiendapp/image/upload/w_400,h_300,c_scale/${props.images[0].imageId}`) : "https://media.istockphoto.com/vectors/no-image-available-sign-vector-id922962354?k=6&m=922962354&s=612x612&w=0&h=_KKNzEwxMkutv-DtQ4f54yA5nc39Ojb_KPvoV__aHyU="} alt='not found' id='image' />
             </div>
             <div className="card-body">
                 <h5 className="card-title">{props.name}</h5>
