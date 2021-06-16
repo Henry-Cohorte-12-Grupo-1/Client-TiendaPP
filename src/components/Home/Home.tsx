84323a1de856a01e90c3b5d040890b234a23297d
= <violetafuentes2002@gmail.com>
2021-06-16 19:28:22 -0300

    estilos de navbar con categorias listas

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,15 +1,14 @@
 import { StoreType } from '../../redux/reducers/index';
-import { bringProducts, getCategories, orderByCategories } from '../../redux/actions/index';
+import { bringProducts, getCategories } from '../../redux/actions/index';
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
-import obj, { category } from '../../interfaces/products';
+import { useEffect } from 'react';
+import obj from '../../interfaces/products';
 import ProductsCards from '../ProductsCards/ProductsCards';
 import './Home.scss'
 import { Container } from 'react-bootstrap';
 
 function Home() {
     const producto = useSelector<StoreType, obj[]>((state) => state.filterProducts)
-    const categorias = useSelector<StoreType, category[]>((s) => s.filter)
     const dispatch = useDispatch()
 
     useEffect(() => {
@@ -17,26 +16,9 @@ function Home() {
         dispatch(getCategories())
     }, []) //eslint-disable-line
 
-    const handleClick = (category: string) => {
-        dispatch(orderByCategories(category))
-    }
     console.log(producto, "producto")
     return (
         <div id='home-container'>
-            <ul>
-                <li className='recomendados'>Categories</li>
-                <ul className='bStyle'>
-                    {categorias &&
-                        <div className='button'>
-                            {categorias.map(c => {
-                                return (
-                                    <li className="btn" onClick={() => handleClick(c.name)}>{c.name}</li>
-                                )
-                            })}
-                            <li className="btn" onClick={() => dispatch(bringProducts())}>Go back</li>
-                        </div>}
-                </ul>
-            </ul>
             <div id='separation'>
                 <Container id="homeContainer" className='d-flex justify-content-center flex-wrap ml-0 mr-0'>
                     {producto &&
