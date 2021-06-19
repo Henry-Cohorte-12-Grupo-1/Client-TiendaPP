e2c6cb5949a0e2e4f61863a4b6aa893dee32eddc
= <violetafuentes2002@gmail.com>
2021-06-19 20:23:47 -0300

    agregue carousel al home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,11 @@
 import { StoreType } from '../../redux/reducers/index';
 import { bringProducts, getCategories } from '../../redux/actions/index';
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import obj from '../../interfaces/products';
 import ProductsCards from '../ProductsCards/ProductsCards';
 import './Home.scss'
-import { Container } from 'react-bootstrap';
+import { Container, Carousel } from 'react-bootstrap';
 
 function Home() {
     const producto = useSelector<StoreType, obj[]>((state) => state.filterProducts)
@@ -16,9 +16,37 @@ function Home() {
         dispatch(getCategories())
     }, []) //eslint-disable-line
 
-    console.log(producto, "producto")
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex: any, e: any) => {
+        setIndex(selectedIndex);
+    }
+
     return (
         <div id='home-container'>
+            <Carousel activeIndex={index} onSelect={handleSelect} id="ContainerC">
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100" id="cContainer"
+                    src="https://ichef.bbci.co.uk/news/976/cpsprodpb/E30B/production/_114032185_onlineshopping.jpg"
+                    alt="First slide"
+                    />
+                    <Carousel.Caption>
+                    <h3>Buy at the best price</h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100" id="cContainer"
+                    src="https://www.selleractive.com/hubfs/Ecommerce%20Tools%201.png"
+                    alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                    <h3>Sell what you no longer use</h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
             <div id='separation'>
                 <Container id="homeContainer" className='d-flex justify-content-center flex-wrap ml-0 mr-0'>
                     {producto &&
