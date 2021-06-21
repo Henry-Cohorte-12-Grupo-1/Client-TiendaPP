41f9d8a07ecd2e7494b25980872dbcd1985d68a9
leandrojg <79561670+leandrojg@users.noreply.github.com>
2021-06-21 02:48:18 -0300

    arreglado api domain

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,4 @@
-const ip = "18.229.137.129";
 export let url = process.env.NODE_ENV === "production" ? 
-`http://${ip}/api`
+`http://${process.env.DOMAIN_ENV}/api`
 :
 "http://localhost:3001/api"
-
-
-
