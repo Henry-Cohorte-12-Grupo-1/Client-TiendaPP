59c2f95dc938a3f8ac7aa46a325dd5f5eea88932
manu naga <nagami.mn@gmail.com>
2021-06-23 15:51:18 -0300

    added overlay to detail prods for arrow visibilty

--- src/components/ProductDetails/ProductDetails.tsx
+++ src/components/ProductDetails/ProductDetails.tsx
@@ -1,15 +1,15 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { StoreType } from '../../redux/reducers';
-import { ReactElement, useEffect, useState } from 'react';
-import { productInfo } from '../../redux/actions';
-import { RouteComponentProps } from 'react-router-dom';
-import detailedProduct from '../../interfaces/detailedProduct';
-import { Carousel, Container } from 'react-bootstrap';
-import './style.scss';
+import { useDispatch, useSelector } from "react-redux";
+import { StoreType } from "../../redux/reducers";
+import { ReactElement, useEffect, useState } from "react";
+import { productInfo } from "../../redux/actions";
+import { RouteComponentProps } from "react-router-dom";
+import detailedProduct from "../../interfaces/detailedProduct";
+import { Carousel, Container } from "react-bootstrap";
+import "./style.scss";
 
 //for the add to cart button
-import AddButton from '../CartButtons/AddButton';
-import jwtDecode from 'jwt-decode';
+import AddButton from "../CartButtons/AddButton";
+import jwtDecode from "jwt-decode";
 
 //defino el tipado para match.params.id
 interface MatchParams {
@@ -18,13 +18,17 @@ interface MatchParams {
 type Props = RouteComponentProps<MatchParams>;
 function ProductDetails(props: Props): ReactElement {
     const id = props.match.params.id;
-    const details = useSelector<StoreType, detailedProduct>((state) => state.productDetails);
+    const details = useSelector<StoreType, detailedProduct>(
+        (state) => state.productDetails
+    );
     const dispatch = useDispatch();
     const [loading, setLoading] = useState(true);
 
     //GETTING USER ID FROM LOCAL STORAGE
-    const token: any = localStorage.token ? jwtDecode(localStorage.token) : false;
-    const userId: string = token ? token.id : 'guest';
+    const token: any = localStorage.token
+        ? jwtDecode(localStorage.token)
+        : false;
+    const userId: string = token ? token.id : "guest";
 
     useEffect(() => {
         (async () => {
@@ -32,7 +36,7 @@ function ProductDetails(props: Props): ReactElement {
             setLoading(false);
         })();
     }, []); // eslint-disable-line react-hooks/exhaustive-deps
-    console.log(details, 'Componente ProductDetails');
+    console.log(details, "Componente ProductDetails");
 
     if (loading) {
         return <h1>Loading...</h1>;
@@ -44,7 +48,10 @@ function ProductDetails(props: Props): ReactElement {
         <Container className="mb-5">
             <div>
                 <h2 className="text-center mt-5 mb-5">{details.name}</h2>
-                <Container className="d-flex flex-column flex-md-row" id="productInfoResponsive">
+                <Container
+                    className="d-flex flex-column flex-md-row"
+                    id="productInfoResponsive"
+                >
                     {details.Images.length > 0 ? (
                         <Carousel>
                             {details.Images.map((img, i) => (
@@ -55,6 +62,7 @@ function ProductDetails(props: Props): ReactElement {
                                         src={`http://res.cloudinary.com/tiendapp/image/upload/w_400,h_300,c_scale/${img.imageId}`}
                                         alt="Slides"
                                     />
+                                    <div className="prod-pic-overlay"> </div>
                                 </Carousel.Item>
                             ))}
                         </Carousel>
@@ -62,17 +70,27 @@ function ProductDetails(props: Props): ReactElement {
                     <Container className="d-flex flex-column">
                         <h4>${details.price}</h4>
                         <p>{details.description}</p>
-                        {details.quantity > 0 ? <AddButton productId={details.productId} userId={userId} /> : <em>Not available right now</em>}
-
+                        {details.quantity > 0 ? (
+                            <AddButton
+                                productId={details.productId}
+                                userId={userId}
+                            />
+                        ) : (
+                            <em>Not available right now</em>
+                        )}
                     </Container>
                 </Container>
                 <hr></hr>
                 <h2 className="text-center mt-4">Reviews</h2>
                 {details.Reviews.length ? (
                     details.Reviews.map((rev) => {
-                        console.log("Reviews --->", rev)
+                        console.log("Reviews --->", rev);
                         return (
-                            <div className="card center" id="spacingReview" key={rev.review}>
+                            <div
+                                className="card center"
+                                id="spacingReview"
+                                key={rev.review}
+                            >
                                 <div className="card-body">
                                     <div className="row">
                                         <div className="col-md-2">
@@ -82,12 +100,16 @@ function ProductDetails(props: Props): ReactElement {
                                                     className="img img-rounded img-fluid"
                                                     alt="person portrait"
                                                 />
-
                                             </div>
                                             <div className="row">
                                                 <p>
-                                                    <a className="ml-5" href="/">
-                                                        <strong>{rev.User?.username}</strong>
+                                                    <a
+                                                        className="ml-5"
+                                                        href="/"
+                                                    >
+                                                        <strong>
+                                                            {rev.User?.username}
+                                                        </strong>
                                                     </a>
                                                 </p>
                                             </div>
@@ -95,7 +117,7 @@ function ProductDetails(props: Props): ReactElement {
                                                 {Array(rev.score).fill(
                                                     <span>
                                                         <i className="text-warning fa fa-star"></i>
-                                                    </span>,
+                                                    </span>
                                                 )}
                                             </div>
                                         </div>
