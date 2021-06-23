1c63a81c55089656dacbea076118ce4652f9e372
manu naga <nagami.mn@gmail.com>
2021-06-22 23:49:10 -0300

    overlay to home carousel for visibility

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,73 +1,93 @@
-import { StoreType } from '../../redux/reducers/index';
-import { bringProducts, getCategories } from '../../redux/actions/index';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
-import obj from '../../interfaces/products';
-import ProductsCards from '../ProductsCards/ProductsCards';
-import './Home.scss'
-import { Container, Carousel } from 'react-bootstrap';
+import { StoreType } from "../../redux/reducers/index";
+import { bringProducts, getCategories } from "../../redux/actions/index";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import obj from "../../interfaces/products";
+import ProductsCards from "../ProductsCards/ProductsCards";
+import "./Home.scss";
+import { Container, Carousel } from "react-bootstrap";
 
 function Home() {
-    const producto = useSelector<StoreType, obj[]>((state) => state.filterProducts)
-    const dispatch = useDispatch()
+    const producto = useSelector<StoreType, obj[]>(
+        (state) => state.filterProducts
+    );
+    const dispatch = useDispatch();
 
     useEffect(() => {
-        dispatch(bringProducts())
-        dispatch(getCategories())
-    }, []) //eslint-disable-line
+        dispatch(bringProducts());
+        dispatch(getCategories());
+    }, []); //eslint-disable-line
 
     const [index, setIndex] = useState(0);
 
-    const handleSelect = (selectedIndex: any, e: any) => {
+    const handleSelect = (selectedIndex: any) => {
         setIndex(selectedIndex);
-    }
+    };
 
     return (
-        <div id='home-container'>
-            <Carousel activeIndex={index} onSelect={handleSelect} id="ContainerC">
+        <div id="home-container">
+            {/*CAROUSEL*/}
+            <Carousel
+                activeIndex={index}
+                onSelect={handleSelect}
+                id="ContainerC"
+            >
+                {/*CAROUSEL ITEM*/}
                 <Carousel.Item>
                     <img
-                    className="d-block w-100" id="cContainer"
-                    src="https://ichef.bbci.co.uk/news/976/cpsprodpb/E30B/production/_114032185_onlineshopping.jpg"
-                    alt="First slide"
+                        className="d-block w-100"
+                        id="cContainer"
+                        src="https://ichef.bbci.co.uk/news/976/cpsprodpb/E30B/production/_114032185_onlineshopping.jpg"
+                        alt="First slide"
                     />
+                    {/*CLAVE PARA EL OVERLAY */}
+                    <div className="overlay"> </div>{" "}
                     <Carousel.Caption>
-                    <h3>Buy at the best price</h3>
+                        <h3>Buy at the best price</h3>
                     </Carousel.Caption>
                 </Carousel.Item>
+
+                {/*CAROUSEL ITEM*/}
                 <Carousel.Item>
                     <img
-                    className="d-block w-100" id="cContainer"
-                    src="https://www.selleractive.com/hubfs/Ecommerce%20Tools%201.png"
-                    alt="Second slide"
+                        className="d-block w-100"
+                        id="cContainer"
+                        src="https://www.selleractive.com/hubfs/Ecommerce%20Tools%201.png"
+                        alt="Second slide"
                     />
 
+                    {/*CLAVE PARA EL OVERLAY */}
+                    <div className="overlay"> </div>
+
                     <Carousel.Caption>
-                    <h3>Sell what you no longer use</h3>
+                        <h3>Sell what you no longer use</h3>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
-            <div id='separation'>
-                <Container id="homeContainer" className='d-flex justify-content-center flex-wrap ml-0 mr-0'>
+
+            <div id="separation">
+                <Container
+                    id="homeContainer"
+                    className="d-flex justify-content-center flex-wrap ml-0 mr-0"
+                >
                     {producto &&
                         // <div className="row row-cols-1 row-cols-md-2">
-                        producto.map(p => {
+                        producto.map((p) => {
                             return (
                                 <ProductsCards
+                                    key={p.productId}
                                     name={p.name}
                                     price={p.price}
                                     productId={p.productId}
                                     image=""
                                     images={p.Images ? p.Images : []}
                                 />
-                            )
-                        })
-                    }
+                            );
+                        })}
                 </Container>
             </div>
         </div>
-    )
+    );
 }
 
-
-export default Home
+export default Home;
