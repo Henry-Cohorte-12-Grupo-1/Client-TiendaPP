b7ec6a2e2e4ef06dab3c356b03d869e0a88c9b7c
Pedro <pedro.maresc@gmail.com>
2021-06-24 01:20:30 -0300

    added link in images

--- src/components/ProductsCards/ProductsCards.tsx
+++ src/components/ProductsCards/ProductsCards.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import WishlistButton from '../Wishlist/Buttons/WishlistButton';
 import './Products.css';
 
@@ -19,16 +20,20 @@ function ProductsCards(props: {
     return (
         <div className="card" id="firstDiv">
             <div id="imageSize">
-                <img
-                    className="card-img-top"
-                    src={
-                        props.images && props.images.length
-                            ? `http://res.cloudinary.com/tiendapp/image/upload/w_400,h_300,c_scale/${props.images[0].imageId}`
-                            : 'https://media.istockphoto.com/vectors/no-image-available-sign-vector-id922962354?k=6&m=922962354&s=612x612&w=0&h=_KKNzEwxMkutv-DtQ4f54yA5nc39Ojb_KPvoV__aHyU='
-                    }
-                    alt="not found"
-                    id="image"
-                />
+                <Link to={`/product/${props?.productId}`}>
+
+                    <img
+
+                        className="card-img-top"
+                        src={
+                            props.images && props.images.length
+                                ? `http://res.cloudinary.com/tiendapp/image/upload/w_400,h_300,c_scale/${props.images[0].imageId}`
+                                : 'https://media.istockphoto.com/vectors/no-image-available-sign-vector-id922962354?k=6&m=922962354&s=612x612&w=0&h=_KKNzEwxMkutv-DtQ4f54yA5nc39Ojb_KPvoV__aHyU='
+                        }
+                        alt="not found"
+                        id="image"
+                    />
+                </Link>
             </div>
             <div className="card-body">
                 <h5 className="card-title">{props.name}</h5>
