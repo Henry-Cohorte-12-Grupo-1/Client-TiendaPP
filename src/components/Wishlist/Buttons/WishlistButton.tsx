5d89fdb010e08dbbdb8501e7f620c810613d9224
leandrojg <79561670+leandrojg@users.noreply.github.com>
2021-06-27 00:51:23 -0300

    fix wishlist

--- src/components/Wishlist/Buttons/WishlistButton.tsx
+++ src/components/Wishlist/Buttons/WishlistButton.tsx
@@ -1,54 +1,48 @@
 import { ReactElement } from "react";
-import swal from 'sweetalert'
-import axios from "axios";
+import { useDispatch } from "react-redux";
+import { bringWishlist } from "../../../redux/actions";
 import { url } from "../../../api";
-import { useHistory } from "react-router";
-import heart from '../../../assets/heart1.svg'
-import heartUnfilled from '../../../assets/heart2.svg'
+
+import axios from "axios";
+import heart from "../../../assets/heart1.svg";
+import heartUnfilled from "../../../assets/heart2.svg";
 
 interface Props {
-    userId?: string;
-    productId: string;
-    isWished?: boolean
+  userId?: any; //arreglar
+  productId: string;
+  isWished?: boolean;
 }
 
 function WishlistButton(props: Props): ReactElement {
+  const dispatch=useDispatch();
+  
+  const { userId, productId } = props;
+  const wishItem = {
+    userId,
+    productId,
+  };
 
-    const history = useHistory()
-    const { userId, productId } = props;
-    const wishItem = {
-        userId,
-        productId
-    }
+  const handleWish = async (wishValue: boolean, ) => {
+    let wishParam = wishValue ? "add" : "delete"; //wishParam deberia ser una tupla. 
+    await axios.post(`${url}/wishlist/${wishParam}`, wishItem);
+    dispatch(bringWishlist(userId));
+  };
 
-    const handleAdd = async (e: any) => {
-        e.preventDefault();
-        const resp = await axios.post(`${url}/wishlist/add`, wishItem);
-        swal(resp.data).then(() => history.go(0))
-    }
-    const handleRemove = async (e: any) => {
-        e.preventDefault();
-        const resp = await axios.post(`${url}/wishlist/delete`, wishItem);
-        swal(resp.data).then(() => history.go(0))
-
-    }
-    if (!userId) {
-        return (
-            <div></div>
-        )
-    }
-    if (props.isWished) {
-        return (
-            <button type="button" className="btn" onClick={handleRemove} >
-                <img src={heart} width="30" height="30" alt="remove heart" />
-            </button>
-        )
-    } else {
-        return (
-            <button type="button" className="btn" onClick={handleAdd}>
-                <img src={heartUnfilled} width="30" height="30" alt="add heart" />
-            </button>
-        )
-    }
+  if (!userId) {
+    return <div></div>;
+  }
+  if (props.isWished) {
+    return (
+      <button type="button" className="btn" onClick={()=>handleWish(false)}>
+        <img src={heart} width="30" height="30" alt="remove heart" />
+      </button>
+    );
+  } else {
+    return (
+      <button type="button" className="btn" onClick={()=>handleWish(true)}>
+        <img src={heartUnfilled} width="30" height="30" alt="add heart" />
+      </button>
+    );
+  }
 }
 export default WishlistButton;
